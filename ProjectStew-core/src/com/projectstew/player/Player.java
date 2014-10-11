0b7fe0934f30f9b309fc095a01116284c4a67a8f
FunnyGopher <FunnyGopher@Hermes>
2014-10-11 13:12:19 -0700

    Added a GamePad to the Player.

---
+++ ProjectStew-core/src/com/projectstew/player/Player.java
@@ -0,0 +1,31 @@
+package com.projectstew.player;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.projectstew.GamePad;
+import com.projectstew.GameTime;
+import com.projectstew.Graphic;
+
+public class Player extends Graphic {
+	
+	private float x, y;
+	private GamePad gamePad;
+	
+	public Player(float x, float y, Texture texture, GamePad gamePad) {
+		super(texture);
+		this.x = x;
+		this.y = y;
+		this.gamePad = gamePad;
+	}
+	
+	@Override
+	public void update(GameTime gameTime) {
+		sprite.translateX(gamePad.getLeftAxis().getX());
+		sprite.translateY(gamePad.getLeftAxis().getY());
+	}
+	
+	@Override
+	public void draw(SpriteBatch batch) {
+		sprite.draw(batch);
+	}
+}
