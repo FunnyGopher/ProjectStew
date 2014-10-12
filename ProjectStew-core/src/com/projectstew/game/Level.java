3f2b1ec1e280cb31e9baabfd5788e9c5aab22b8c
JackKell <Jackkell@Citadel>
2014-10-11 18:22:26 -0700

    Making a Level or Map

---
+++ ProjectStew-core/src/com/projectstew/game/Level.java
@@ -0,0 +1,39 @@
+package com.projectstew.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.projectstew.GameObject;
+
+public class Level implements GameObject{
+	private String tmxFileName;
+	private TiledMap tiledMap;
+	private OrthographicCamera camera;
+	private TiledMapRenderer tiledMapRenderer;
+		
+	public Level(String tmxFileName) {
+		this.tmxFileName = tmxFileName;
+		this.camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1000, 1000);
+		camera.update();
+		this.tiledMap = new TmxMapLoader().load(this.tmxFileName);
+		this.tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+	}
+
+	@Override
+	public void update(GameTime gameTime) {
+		camera.update();
+		
+	}
+
+	@Override
+	public void draw(SpriteBatch spriteBatch) {
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+	}
+}
